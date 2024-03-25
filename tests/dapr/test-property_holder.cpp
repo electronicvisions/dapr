b106ded59a059fef8e60d8bc954c3fd351f56253
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2024-03-25 10:00:41 +0100

    feat: Add property holder for polymorphic objects
    - split out of b1ad4bafbcfa276b2b486b2eac4b61e24ab502a3 in grenade
    
    Change-Id: I1c8b3591f33d17f4560ed84208739d22db024535

---
+++ tests/dapr/test-property_holder.cpp
@@ -0,0 +1,78 @@
+#include "dapr/property_holder.h"
+
+#include "dapr/empty_property.h"
+#include "dapr/property.h"
+#include <memory>
+#include <gtest/gtest.h>
+
+
+namespace {
+
+struct DummyProperty : public dapr::Property<DummyProperty>
+{
+	virtual ~DummyProperty() {}
+};
+
+
+struct DerivedDummyProperty : public DummyProperty
+{
+	int value;
+
+	DerivedDummyProperty(int value) : DummyProperty(), value(value) {}
+
+	virtual std::unique_ptr<DummyProperty> copy() const override
+	{
+		return std::make_unique<DerivedDummyProperty>(*this);
+	}
+
+	virtual std::unique_ptr<DummyProperty> move() override
+	{
+		return std::make_unique<DerivedDummyProperty>(std::move(*this));
+	}
+
+	virtual std::ostream& print(std::ostream& os) const override
+	{
+		return os << "DerivedDummyProperty(" << value << ")";
+	}
+
+protected:
+	virtual bool is_equal_to(DummyProperty const& other) const override
+	{
+		return value == static_cast<DerivedDummyProperty const&>(other).value;
+	}
+};
+
+} // namespace
+
+
+TEST(PropertyHolder, General)
+{
+	DerivedDummyProperty derived_dummy_5{5};
+	dapr::PropertyHolder<DummyProperty> holder_5(derived_dummy_5);
+
+	EXPECT_TRUE(holder_5);
+	EXPECT_NO_THROW(*holder_5);
+	EXPECT_NO_THROW(holder_5.operator->());
+
+	std::stringstream ss;
+	ss << holder_5;
+	EXPECT_EQ(ss.str(), "DerivedDummyProperty(5)");
+
+	EXPECT_EQ(holder_5, holder_5);
+
+	auto const holder_5_move = std::move(holder_5);
+
+	EXPECT_NE(holder_5, holder_5_move);
+	EXPECT_FALSE(holder_5);
+	EXPECT_THROW(*holder_5, std::runtime_error);
+	EXPECT_THROW(holder_5.operator->(), std::runtime_error);
+	EXPECT_EQ(holder_5, holder_5);
+
+	DerivedDummyProperty derived_dummy_7{7};
+	dapr::PropertyHolder<DummyProperty> holder_7(std::move(derived_dummy_7));
+
+	auto const holder_7_copy = holder_7;
+
+	EXPECT_NE(holder_5_move, holder_7);
+	EXPECT_EQ(holder_7_copy, holder_7);
+}
